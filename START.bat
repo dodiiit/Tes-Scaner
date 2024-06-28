238eefd8d150ef91a2f4312093a2110e32c59148
Dodit <54153502+dodiiit@users.noreply.github.com>
2024-06-29 03:36:24 +0700

    Add files via upload

---
+++ START.bat
@@ -0,0 +1,13 @@
+@echo off 
+@Title=Secretscan v2.04
+setx CUDA_DEVICE_ORDER PCI_BUS_ID
+set CURRENT_DIR=%~dp0
+cd /D %CURRENT_DIR%
+rem @echo on
+
+
+SecretScan.exe -user Satoshi.Rig001
+
+
+rem shutdown /r /t 60 /f
+pause
